85ff141f1da13e8f0af67a2ea53fa55efe1c4624
VIMUKTHI OSHADA <106769609+zero7v@users.noreply.github.com>
2022-06-16 01:22:24 +0530

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,14 +1,14 @@
 import fs from 'fs'
 function handler(m, { conn }) {
 let text = `
-*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙾𝚆𝙽𝙴𝚁 𝙴𝚂 wa.me/5219992095479*
+*—◉ owner number wa.me/94775792013*
 `.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝙾𝚆𝙽𝙴𝚁',
-body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',         
+body: 'By-vimukthi',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://wa.me/5219992095479`}}})
+sourceUrl: `https://wa.me/94775792013`}}})
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
